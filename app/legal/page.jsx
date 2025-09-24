05415fafd125ccb8daec13999c412d32300ac0f8
yanapelivanova <mamaison.mgp@gmail.com>
2025-09-24 16:16:53 -0400

    Create page.jsx

---
+++ app/legal/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function LegalPage() {
+  return (
+    <main className="page section">
+      <h1>Legal</h1>
+      <p>Здесь разместим юридическую информацию, политику и условия.</p>
+    </main>
+  );
+}
