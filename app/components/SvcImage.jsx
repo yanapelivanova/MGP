32424d64172a53c2191072e5fbea84b988e1180b
yanapelivanova <mamaison.mgp@gmail.com>
2025-12-08 13:10:49 -0500

    Update SvcImage.jsx

--- app/components/SvcImage.jsx
+++ app/components/SvcImage.jsx
@@ -1,23 +1,21 @@
-// app/components/SvcImage.jsx
 "use client";
 
 import Image from "next/image";
 
 export default function SvcImage({ src, alt, className = "" }) {
   return (
-    <div className={`svc-img ${className}`}>
-      {/* Картинка */}
+    <div className={`relative w-full svc-soft-wrap ${className}`}>
       <Image
         src={src}
         alt={alt}
-        fill
-        sizes="(max-width: 1100px) 100vw, 540px"
-        className="svc-img-el"
+        width={1200}
+        height={675}
+        className="object-contain w-full h-auto rounded-xl z-[2] relative"
         priority={false}
       />
 
-      {/* Мягкое свечение + пульсация по краю */}
-      <div className="svc-img-glow" aria-hidden="true" />
+      {/* Soft glow */}
+      <div className="svc-soft-glow" aria-hidden="true" />
     </div>
   );
 }
