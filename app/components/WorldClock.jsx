493d9b7702f0324c512bd53d43e3da92a0505026
yanapelivanova <mamaison.mgp@gmail.com>
2026-01-12 12:12:42 -0500

    Refactor WorldClock and Clock components

--- app/components/WorldClock.jsx
+++ app/components/WorldClock.jsx
@@ -2,19 +2,35 @@
 
 export default function WorldClock() {
   return (
-    <div className="world-clock-wrap" aria-hidden="true">
+    <section className="world-clocks" aria-hidden="true">
+      <Clock city="Montreal" />
+      <Clock city="London" />
+      <Clock city="Hong Kong" />
+    </section>
+  );
+}
+
+function Clock({ city }) {
+  return (
+    <div className="clock-item">
       <div className="clock-face">
         {/* метки */}
-        <div className="clock-marks" />
+        <div className="clock-marks">
+          {Array.from({ length: 60 }).map((_, i) => (
+            <span key={i} style={{ transform: `rotate(${i * 6}deg)` }} />
+          ))}
+        </div>
 
         {/* световая пыль */}
         <div className="clock-glow" />
 
         {/* стрелки */}
-        <div className="hand hour" />
-        <div className="hand minute" />
-        <div className="hand second" />
+        <div className="clock-hand hand-hour" />
+        <div className="clock-hand hand-minute" />
+        <div className="clock-hand hand-second" />
       </div>
+
+      <div className="clock-city">{city}</div>
     </div>
   );
 }
